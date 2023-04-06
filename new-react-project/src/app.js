7cb8585c145fd8d7c0565596c8265bfcd47fa627
Manny <mannysth@yahoo.com>
2023-04-06 10:45:57 -0700

    initializes project

---
+++ new-react-project/src/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='conatainer'>
+                <h1>Helloo there</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
